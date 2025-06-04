00fb69b0be91c6fcb6d8b2861328b059ef9d9b6c
oliynykmax <olejnikmax@gmail.com>
2025-06-04 19:44:08 +0300

    complete, c++20

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -13,6 +13,7 @@ int	main(int ac, char **av)
 			{
 				std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(av[i][j])));
 			}
+		}
 		std::cout << std::endl;
 	}
 	return (0);
--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -3,6 +3,11 @@
 #include <chrono>
 #include <iomanip>
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
 
 Account::Account(int initial_deposit) : _accountIndex(_nbAccounts++), _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) {
     _totalAmount += initial_deposit;
@@ -10,7 +15,10 @@ Account::Account(int initial_deposit) : _accountIndex(_nbAccounts++), _amount(in
     std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
 }
 
-Account::~Account(){}
+Account::~Account(){
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
 
 int Account::getNbAccounts() { return _nbAccounts; }
 
@@ -22,7 +30,10 @@ int Account::getNbWithdrawals() { return _totalNbWithdrawals; }
 
 void Account::displayAccountsInfos() {
     _displayTimestamp();
-    std::cout << "accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+    std::cout << "accounts:" << getNbAccounts()
+              << ";total:" << getTotalAmount()
+              << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 void Account::makeDeposit(int deposit) {
