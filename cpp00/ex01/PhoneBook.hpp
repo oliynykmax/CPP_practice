1bd46008724d0befb28e2d10218c32b6bec17c4c
oliynykmax <olejnikmax@gmail.com>
2025-06-04 13:40:48 +0300

    ex01/cpp00

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -5,18 +5,19 @@
 
 class PhoneBook {
 private:
-	Contact contacts[8]; // Array to hold up to 8 contacts
-	int currentIndex;    // Index for the next contact to be added
-	int totalContacts;   // Total number of contacts added
+	Contact _contacts[8];
+	int currentIndex;
+	int totalContacts;
+	std::string _truncate(const std::string& str) const;
 
-	
+
 public:
     PhoneBook();
     ~PhoneBook();
 	void addContact();
 	void searchContacts() const;
 	void displayContact(int index) const;
-    
+
 };
 
 #endif
