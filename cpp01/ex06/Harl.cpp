3bd5847fe196011490a11d5262368823bb04eace
oliynykmax <olejnikmax@gmail.com>
2025-06-05 19:22:04 +0300

    Harl filter

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -32,7 +32,7 @@ void Harl::info(void)
 
 void Harl::warning(void)
 {
-	std::cout << "I think I deserve to have some extra bacon for free. \nI've been coming here for years and you just started working here last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. \n:wqI've been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Harl::error(void)
