44374a7de335ef383e326ba10b93f849b301d331
Max <olejnikmax@gmail.com>
2025-10-10 11:57:11 +0300

    ex02

---
+++ cpp06/ex01/inc/Data.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+class Data {
+    private:
+    int numbers;
+    double details;
+
+    public:
+    Data() = default;
+    ~Data() = default;
+    Data(int num, double details);
+    Data(const Data &other) = default;
+    Data &operator=(const Data &other) = default;
+};
---
+++ cpp06/ex01/src/Data.cpp
@@ -0,0 +1,3 @@
+#include "../inc/Data.hpp"
+
+Data::Data(int num, double det) : numbers(num), details(det) {}
