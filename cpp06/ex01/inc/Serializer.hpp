8f41351e2747e77dbc342ba9c856b10b04260f8e
Max <olejnikmax@gmail.com>
2025-10-11 22:56:17 +0300

    final fix

--- cpp06/ex01/inc/Serializer.hpp
+++ cpp06/ex01/inc/Serializer.hpp
@@ -1,14 +1,15 @@
 #pragma once
-#include <cstdint>
 #include "Data.hpp"
+#include <cstdint>
 
 class Serializer {
-    private:
-    Serializer() = delete;
-    ~Serializer() = delete;
-    Serializer(const Serializer &other) = delete;
-    Serializer &operator=(const Serializer &other) = delete;
-    public:
-    static uintptr_t serialize(Data* ptr);
-    static Data* deserialize(uintptr_t raw);
+private:
+  Serializer() = delete;
+  ~Serializer() = delete;
+  Serializer(const Serializer &other) = delete;
+  Serializer &operator=(const Serializer &other) = delete;
+
+public:
+  static uintptr_t serialize(Data *ptr);
+  static Data *deserialize(uintptr_t raw);
 };
