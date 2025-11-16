4f4a39b173b88a5a2f53b5c2eeb83c9a32f6cb4f
oliynykmax <olejnikmax@gmail.com>
2025-11-16 15:12:50 +0200

    step one of mergesort

--- cpp09/ex02/src/PmergeMe.cpp
+++ cpp09/ex02/src/PmergeMe.cpp
@@ -3,13 +3,13 @@
 std::vector<int> sort_vector(int argc, char **argv) {
   std::vector<int> vec = parseAndLoad<std::vector<int>>(argc, argv);
 
-  std::sort(vec.begin(), vec.end());
+  sort_fj(vec);
   return vec;
 }
 
 std::deque<int> sort_deque(int argc, char **argv) {
   std::deque<int> deq = parseAndLoad<std::deque<int>>(argc, argv);
 
-  std::sort(deq.begin(), deq.end());
+  sort_fj(deq);
   return deq;
 }
