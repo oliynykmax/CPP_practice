bb5e962aa73369ae5656232df9c10501ffa97cde
oliynykmax <olejnikmax@gmail.com>
2026-01-07 16:48:10 +0200

    bts fix

--- cpp09/ex00/src/main.cpp
+++ cpp09/ex00/src/main.cpp
@@ -1,23 +1,18 @@
-#include <iostream>
 #include "../inc/BitcoinExchange.hpp"
+#include <iostream>
 
-int main(int argc, char **argv)
-{
-    (void)argv;
-    if (argc != 2)
-    {
-        std::cerr << "Error: could not open file." << std::endl;
-        return 1;
-    }
+int main(int argc, char **argv) {
+  (void)argv;
+  if (argc != 2) {
+    std::cerr << "Error: could not open file." << std::endl;
+    return 1;
+  }
+  try {
     BitcoinExchange db;
-    try
-    {
-        db.processFile(argv[1]);
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
-    return 0;
+    db.processFile(argv[1]);
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
 }
